a8ccacd9ad395e07fe23cd32cd1f97d4bc95605c
Steffen Hansen <stefh14@student.sdu.dk>
2016-12-30 15:18:41 +0100

    Separated REST into it's own module

--- server.js
+++ server.js
@@ -1,35 +1,5 @@
-var fs = require("fs");
-var http = require("http");
-const PORT = 9998;
+console.log("Starting Bidding.io server...");
 
-var contents = fs.readFileSync("content.json");
-var contentsJson = JSON.parse(contents);
+var rest_module = require("./rest.js");
 
-send_header = function (response, responseCode) {
-    response.writeHead(responseCode, {'Content-Type': 'application/json',
-                             'Access-Control-Allow-Origin': '*',
-                             'Access-Control-Allow-Methods': 'GET,PUT,POST'});
-	console.log("response code " + responseCode);
-}
-
-handler_get = function (request, response) {
-    send_header(response, 200);
-    if(request.url == "/auctions") {
-        response.end(JSON.stringify(contentsJson));
-    } else {
-        response.end("{}");
-    }
-};
-
-dispatch = {
-    'GET': handler_get
-};
-
-var server = http.createServer(function (request, response){
-    console.log(request['method']+' '+request.url);
-    dispatch[request['method']](request, response);
-});
-
-server.listen(PORT, function(){
-    console.log("Listening to http://localhost:%s", PORT);
-});
+rest_module.run(9998);

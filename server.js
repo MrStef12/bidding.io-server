c7c0b9817950587ef29107730a7a0889b3c87909
Steffen Hansen <stefh14@student.sdu.dk>
2017-01-02 18:21:21 +0100

    Moved everything back into one file. Due to lack of method to share data between files.

--- server.js
+++ server.js
@@ -1,5 +1,46 @@
 console.log("Starting Bidding.io server...");
 
-var rest_module = require("./rest.js");
+/// ----------------- GENERAL ----------------- ///
 
-rest_module.run(9998);
+var fs = require("fs");
+var http = require("http");
+var contents = fs.readFileSync("content.json");
+var contentsJson = JSON.parse(contents);
+
+const PORT = 9998;
+
+/// ----------------- REST SERVER ----------------- ///
+
+send_header = function (response, responseCode) {
+    response.writeHead(responseCode, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET'
+    });
+    console.log("REST - response code " + responseCode);
+}
+
+handler_get = function (request, response) {
+    send_header(response, 200);
+    if (request.url == "/auctions") {
+        response.end(JSON.stringify(contentsJson));
+    } else {
+        response.end("{}");
+    }
+};
+
+dispatch = {
+    'GET': handler_get
+};
+
+var server = http.createServer(function (request, response) {
+    console.log("REST - " + request['method'] + " " + request.url);
+    dispatch[request['method']](request, response);
+});
+
+server.listen(PORT, function () {
+    console.log("REST - Listening on http://localhost:%s", PORT);
+});
+
+/// ----------------- WEBSOCKET SERVER ----------------- ///
+
